ca8033c7cb11b5cc70e5e17e872c6b3354c1df4d
rex <rexmac@qq.com>
2023-05-15 17:58:46 +1000

    "create a mapreduce view for sports data"

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -15,8 +15,8 @@ url = f'http://{admin}:{password}@172.26.135.221:5984/'
 couch = couchdb.Server(url)
 
 # indicate the db name
-total_sport_facility_suburb_db = couch['total_number_of_facility_suburb']
-sport_facility_suburb_db = couch['']
+# total_sport_facility_suburb_db = couch['total_number_of_facility_suburb']
+sport_facility_suburb_db = couch['sports_facility_suburb']
 internet_db = couch['']
 public_transport_db = couch['']
 employment_db = couch['']
@@ -32,8 +32,9 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-@app.route('/get_sport_total/<param>', methods=['GET'])
-def get_sport_total(param):
+# Get the data of a specific suburb
+@app.route('/get_sport_specific/<param>', methods=['GET'])
+def get_sport_specific(param):
     # Mango Queries
     query = {
         "selector": {
@@ -42,11 +43,56 @@ def get_sport_total(param):
     }
     # Execute the query
     results = []
-    docs = total_sport_facility_suburb_db.find(query)
+    docs = sport_facility_suburb_db.find(query)
     for row in docs:
         results.append(row)
     # Return the results as JSON
     return {'data': results}
+
+
+# Map function
+get_sport_total_map_func = '''function(doc) {
+    if (doc.Suburb) {
+        var total = 0;
+        for (var facility in doc) {
+            if (facility != "Suburb" && facility != "_id" && facility != "_rev" && typeof doc[facility] == "number") {
+                total += doc[facility];
+            }
+        }
+        emit(doc.Suburb, total);
+    }
+}'''
+
+# Reduce function
+get_sport_total_reduce_func = '_sum'
+
+# Create the view
+# Check if the design document already exists
+design_doc_id = "_design/total_facilities_by_suburb_view"
+if design_doc_id in sport_facility_suburb_db:
+    print("Design document already exists.")
+else:
+    print("Design document does not exist. Creating it now.")
+    # Create the view
+    sport_facility_suburb_db.save({
+        "_id": design_doc_id,
+        "views": {
+            "by_suburb": {
+                "map": get_sport_total_map_func,
+                "reduce": get_sport_total_reduce_func
+            }
+        }
+    })
+
+@app.route('/get_sport_total/<param>', methods=['GET'])
+def get_sport_total(param):
+    # Get the total facilities for a suburb
+    result = sport_facility_suburb_db.view('total_facilities_by_suburb_view/by_suburb', key=param)
+    total = 0
+    for row in result:
+        total = row.value
+    # Return the result as JSON
+    return {'Suburb': param, 'total_facilities': total}
 
 
 @app.route('/internet_db/<param>', methods=['GET'])
@@ -183,6 +229,7 @@ def mastodon_db(param):
         results.append(row)
     # Return the results as JSON
     return {'data': results}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
